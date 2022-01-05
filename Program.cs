892220a03ad2e351390a135a2a8e3be1e93b0433
Wesley Sieiro Takatsu de Araujo <23043537+wesleytakatsu@users.noreply.github.com>
2022-01-05 15:46:14 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PrimeiroCSharpOlaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = "Olá, ";
+            var b = "mundo!";
+            Console.WriteLine(a + b);
+            Console.WriteLine("Olá, mundo!");
+        }
+    }
+}
